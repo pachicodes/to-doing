3dc69925544ea05ed070d116504a0b927bd8e577
pachicodes <pachicarlson@gmail.com>
2019-08-05 21:10:26 -0400

    js started

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 let name = prompt("Please enter your name");
 let span = document.querySelector("[data-span]");
-console.log(span);
-
 span.innerHTML = `${name}`;
 
+let task = document.querySelector("[data-task]");
+let btn = document.querySelector("[data-btn]");
+let list = document.querySelector("[data-list]");
+let todo = [];
+
+btn.addEventListener('click', function() {
+    todo.push(task.value);
+    list.innerHTML = `${todo}`;
+    
+
+
+} )
 
 
