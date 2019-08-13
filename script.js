59a843f6b22d284e21e4d2eeb6f38e939f7abcec
Pachi <pachicodes@gmail.com>
2019-08-13 07:14:57 -0400

    fixing with all

--- script.js
+++ script.js
@@ -9,31 +9,15 @@ let list = document.querySelector("[data-list]");
 btn.addEventListener('click', function() {
     let value = task.value;
     list.insertAdjacentHTML('afterbegin', `<li><input data-check type="checkbox"> ${value} <button data-xbtn class="todo__btn">x</button></li>`);
-    let xbtn = Array.from(documen.querySelector('[data-xbtn]'));
+    
+    let xbtn = Array.from(document.querySelectorAll('[data-xbtn]'));
     xbtn.map(button => {
         button.addEventListener('click', function(event) {
             event.target.parentNode.remove()
         })
     });
-    
+
     task.value = '';
     task.focus();    
 } 
 )
-
-let check = document.querySelector("[data-check]");
-let xbtn = document.querySelector("[data-xbtn]");
-
-/*
-check.addEventListener('click', function() {
-    //create function that will cross item
-
-}
-)
-
-xbtn.addEventListener('click', function() {
-    //create function that will delete item
-})
-
-*/
-
